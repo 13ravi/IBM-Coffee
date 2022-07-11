0f2ebd346ee2d41efbb31e00d3493e979ca21897
13ravi <ravindersinghs@outlook.com>
2022-07-11 17:30:25 -0400

    Created topmenu component

--- IBM-Coffee-FrontEnd/src/app/layout/layout.module.ts
+++ IBM-Coffee-FrontEnd/src/app/layout/layout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopmenuComponent } from './topmenu/topmenu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TopmenuComponent
+  ],
   imports: [
     CommonModule
   ]
