a7597e067dd4b778cdd29a585a61dfa2f4285103
13ravi <ravindersinghs@outlook.com>
2022-07-11 17:31:15 -0400

    Created leftnav component

--- IBM-Coffee-FrontEnd/src/app/layout/layout.module.ts
+++ IBM-Coffee-FrontEnd/src/app/layout/layout.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopmenuComponent } from './topmenu/topmenu.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
 
 
 
 @NgModule({
   declarations: [
-    TopmenuComponent
+    TopmenuComponent,
+    LeftnavComponent
   ],
   imports: [
     CommonModule
