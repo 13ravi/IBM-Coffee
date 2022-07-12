2d1960df72853d644c10d8319c5cb12366912079
13ravi <ravindersinghs@outlook.com>
2022-07-11 22:35:12 -0400

    Updated Layout with some adjustments

--- IBM-Coffee-FrontEnd/src/app/app-routing.module.ts
+++ IBM-Coffee-FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListCoffeeComponent } from './coffee/list-coffee/list-coffee.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'',component:ListCoffeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- IBM-Coffee-FrontEnd/src/app/layout/layout.module.ts
+++ IBM-Coffee-FrontEnd/src/app/layout/layout.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { TopmenuComponent } from './topmenu/topmenu.component';
 import { LeftnavComponent } from './leftnav/leftnav.component';
 import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +14,9 @@ import {MatListModule} from '@angular/material/list';
   ],
   imports: [
     CommonModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   exports:[
     TopmenuComponent,
